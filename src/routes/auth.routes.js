e83a527591d138cc6da13125601f6de946f281ed
neil <neilaular01@protonmail.com>
2024-06-30 13:50:15 -0400

    validate user roles and

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,28 +1,31 @@
 import { Router } from 'express';
 import authService from '../services/auth.service.js';
 
+
 const authRoutes = Router();
+
 
 authRoutes.post('/login', async (req, res) => {
     try {
-        const { email, password, isAdmin } = req.body;
-        
-        // Agregar logs para depuración
-        console.log('Request Body:', req.body);
-        console.log('Email:', email);
-        console.log('Password:', password);
-        console.log('isAdmin:', isAdmin);
-        
-        const result = await authService.login(email, password, isAdmin);
-        
+        const { email, password, role } = req.body;
+
+        // Validar que el rol sea uno de los permitidos
+        if (![role.USER, role.SELLER, role.ADMIN].includes(role)) {
+            return res.status(400).send('Invalid role');
+        }
+
+        // Iniciar sesión
+        const result = await authService.login({ email, password, role });
+
         // Agregar logs para depuración
         console.log('Login Result:', result);
-        
+
         res.send(result);
     } catch (error) {
-        console.error('Error:', error);
-        res.status(401).send('Invalid email or password');
+        console.error('Error during login:', error);
+        res.status(401).send('Invalid email, password, or role');
     }
 });
+
 
 export default authRoutes;
