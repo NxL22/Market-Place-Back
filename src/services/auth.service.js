2c7acd1ba815414d97835a71dcb581ec11638085
neil <neilaular01@protonmail.com>
2024-06-30 13:54:32 -0400

    Add login logic with role-based authentication in Auth service

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,41 +1,70 @@
 import bcrypt from 'bcrypt';
 import dotenv from "dotenv";
-import jwt from 'jsonwebtoken';
 import AdminEntity from '../entities/admin.entity.js';
 import UserEntity from '../entities/user.entity.js';
 import EncriptionFunc from '../utils/jwt.encription.js'
+import { roles } from '../utils/enum/role-enum.js';
+
 
 dotenv.config();
 
-const secretKey = process.env.JWT_SECRET_KEY; 
+const secretKey = process.env.JWT_SECRET_KEY;
 
 class AuthService {
 
-    async login(email, password, isAdmin) {
-        let user;
-        if (isAdmin) {
-            user = await AdminEntity.findOne({ where: { email } });
-            console.log('Admin encontrado:', user); // Añadir log para ver si el admin se encuentra
-            if (!user || !await bcrypt.compare(password, user.password)) {
-                throw new Error('Invalid email or password');
-            }
-        } else {
-            user = await UserEntity.findOne({ where: { email } });
-            console.log('Usuario encontrado:', user); // Añadir log para ver si el usuario se encuentra
-            if (!user || !await bcrypt.compare(password, user.password)) {
-                throw new Error('Invalid email or password');
-            }
+    async login(data) {
+        const { email, password, role } = data;
+
+        if (!email || !password || !role) {
+            throw new Error('Email, password, and role are required');
         }
 
-        const role = isAdmin ? 'admin' : 'user';
-        const token = EncriptionFunc.generateToken({ id: user.id, email: user.email, role });
-        
-        console.log('Generated Token:', token); // Añadir log para ver el token generado
-        
-        return { token, user };
+        try {
+            let user;
+
+            if (email === process.env.EMAIL_ADMIN) {
+                user = await AdminEntity.findOne({ where: { email } });
+                console.log('Admin found:', user);
+                if (!user || !await bcrypt.compare(password, user.password)) {
+                    throw new Error('Invalid email or password');
+                }
+            } else {
+                if (![roles.SELLER, roles.USER].includes(role)) {
+                    throw new Error('Invalid role');
+                }
+
+                if (role === roles.SELLER) {
+                    user = await AdminEntity.findOne({ where: { email } });
+                    console.log('Seller found:', user);
+                    if (!user || !await bcrypt.compare(password, user.password)) {
+                        throw new Error('Invalid email or password');
+                    }
+                } else {
+                    user = await UserEntity.findOne({ where: { email } });
+                    console.log('User found:', user);
+                    if (!user || !await bcrypt.compare(password, user.password)) {
+                        throw new Error('Invalid email or password');
+                    }
+                }
+            }
+
+            const token = EncriptionFunc.generateToken({ id: user.id, email: user.email, role });
+            console.log('Generated Token:', token);
+
+            // Clonamos y a ese objeto le asignamos password = ""
+            const userSafe = { ...user.toJSON(), password: '' };
+
+            return { token, user: userSafe };
+
+        } catch (error) {
+            console.error('Error during login:', error.message);
+            throw new Error('Login failed. Please try again later.');
+        }
     }
 
 }
+
+
 
 
 export default new AuthService();
