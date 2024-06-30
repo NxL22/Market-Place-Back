11c25fd08011d96d3cafd866216fe6d78c111487
neil <neilaular01@protonmail.com>
2024-06-30 13:36:00 -0400

    Add relationships between SellerEntity and ProductEntity in main.js

--- main.js
+++ main.js
@@ -1,34 +1,61 @@
-import sequelize from "./src/config/db.js";
-import httpServer from "./src/config/http.js";
-import dotenv from "dotenv";
+import sequelize from './src/config/db.js';
+import httpServer from './src/config/http.js';
+import dotenv from 'dotenv';
 import './src/config/firebase.js'; 
-
+import adminService from './src/services/admin.service.js';
+import SellerEntity from './src/entities/seller.entity.js';
+import ProductEntity from './src/entities/product.entity.js';
 
 dotenv.config();
 
-async function bootstrap() {
-    const PORT = process.env.PORT || 3000;
-
+async function defineRelationships() {
     try {
-        
+        // Tiene que ser las relaciones antes que hagas el llamado a la BD sequelize.sync()
+        SellerEntity.hasMany(ProductEntity, { foreignKey: 'sellerId', onDelete: 'CASCADE' });
+        ProductEntity.belongsTo(SellerEntity, { foreignKey: 'sellerId' });
+        console.log('Relationships defined successfully.');
+    } catch (relationError) {
+        console.error('Error defining relationships:', relationError);
+        throw relationError; 
+    }
+}
+
+async function initializeFirebase() {
+    try {
+        console.log('Firebase admin initialized successfully.');
+    } catch (error) {
+        console.error('Error initializing Firebase:', error);
+        throw error;
+    }
+}
+
+async function createAdmin() {
+    try {
+        await adminService.createAdmin({ email: process.env.EMAIL_ADMIN, password: process.env.PASSWORD_ADMIN });
+    } catch (error) {
+        console.error('Error creating admin:', error);
+        throw error;
+    }
+}
+
+async function startServer() {
+    const PORT = process.env.PORT || 3000;
+    try {
+        // hacer el llamado a las relaciones antes de sincronizar la base de datos
+        await defineRelationships();
         await sequelize.sync({ alter: true });
-        console.log('Conexión a la base de datos establecida correctamente.');
-        
-        // Inicia el servidor HTTP
-        httpServer.listen(PORT, () => {
-            console.log(`Servidor escuchando en el puerto ${PORT}`);
+        console.log('Database connection established successfully.');
 
-            // Verifica la conexión a Firebase
-            try {
-                console.log('Firebase admin initialized successfully.');
-            } catch (error) {
-                console.error('Error initializing Firebase:', error);
-            }
+        // Start HTTP server
+        httpServer.listen(PORT, async () => {
+            console.log(`Server listening on port ${PORT}`);
+
+            await initializeFirebase();
+            await createAdmin();
         });
-
     } catch (error) {
         console.error('Error during initialization:', error);
     }
 }
 
-bootstrap();
+startServer();
